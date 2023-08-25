b585c027e0b865635e11b03503b8ac4edf5055b9
Uzoezi <uodueme@gmail.com>
2023-08-25 05:01:20 +0100

    made changes to auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -11,7 +11,7 @@ import {
 import {redisClient} from '../utils/connectRedis';
 import { signJwt, verifyJwt } from '../utils/jwt';
 import { User } from '../entity/User';
-import { CreateUserInput } from "../schemas/user.schema";
+import { CreateUserInput, LoginUserInput } from "../schemas/user.schema";
 
 const cookiesOptions: CookieOptions = {
     httpOnly: true,
@@ -69,3 +69,114 @@ const accessTokenCookieOptions: CookieOptions = {
     }
 
   }
+
+
+  export const LoginUserHandler = async (
+    req: Request<{}, {}, LoginUserInput>,
+    res: Response,
+    next: NextFunction
+  ) =>{
+    try{
+      const {email, password} = req.body
+      const user = findUserByEmail({email})
+
+      if(!user || !(await User.comparePasswords(password,(await user).password))){
+        return next(res.status(400).json('Invalid Email or password'));
+      }
+
+      const {access_token, refresh_token} = await signTokens(await user) 
+
+      res.cookie('access_token', access_token, accessTokenCookieOptions)
+      res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions)
+      res.cookie('logged_in', true, {
+        ...accessTokenCookieOptions,
+        httpOnly: false
+      })
+
+      res.status(200).json({
+        status: 'Success',
+        access_token
+      })
+    } catch(err){
+      next(err)
+    }
+  }
+
+    export const refreshAccessTokenHandler = async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      try{
+        const refresh_token = req.cookies.refresh_token
+
+        const message = 'Could not refresh token'
+        if(!refresh_token){
+          return next(res.status(403).json(message))
+        }
+
+        const decoded = verifyJwt<{sub: String}>(
+          refresh_token,
+          'refreshTokenPublicKey'
+        )
+
+        if(!decoded){
+          return next(res.status(403).json(message))
+        }
+
+        const session = await redisClient.get(decoded.sub);
+
+        if(!session){
+          return next(res.status(403).json(message))
+        }
+
+        const user = await findUserById(JSON.parse(session).id);
+
+        if(!user){
+          return next(res.status(403).json(message))
+        }
+
+        const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
+          expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+        });
+
+        res.cookie('access_token', access_token, accessTokenCookieOptions);
+        res.cookie('logged_in', true, {
+          ...accessTokenCookieOptions,
+          httpOnly: false,
+        });
+
+        res.status(200).json({
+          status: 'success',
+          access_token,
+        });
+    
+      } catch(err){
+        next(err);
+      }
+    }
+
+    const logout = (res: Response) => {
+      res.cookie('access_token', '', { maxAge: -1 });
+      res.cookie('refresh_token', '', { maxAge: -1 });
+      res.cookie('logged_in', '', { maxAge: -1 });
+    };
+    
+    export const logoutHandler = async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      try {
+        const user = res.locals.user;
+    
+        await redisClient.del(user.id);
+        logout(res);
+    
+        res.status(200).json({
+          status: 'success',
+        });
+      } catch (err: any) {
+        next(err);
+      }
+    };
